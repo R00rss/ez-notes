c69a5b5800f8b0174b28af43f5fc2247714bb90c
Ronny Garcia <91511334+R00rss@users.noreply.github.com>
2023-03-12 12:34:39 -0500

    Delete backend directory

--- backend/auth/schemas.py
+++
@@ -1,7 +0,0 @@
-# from ..schemas import General_Response_Model
-
-# class Login_Response(General_Response_Model):
-#     status: Status
-#     message: str
-#     error: str | None
-#     payload: any | None
--- backend/database/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .database import *
-# from .models import *
-# from .schemas import *
-# from .crud import *
-
-# from .main import *
